8dcc665fe2f29844e9683e5ede09c4a935cb88e9
Paul Golmann <mail@pje-web.de>
2024-10-31 17:56:34 +0100

    testing nimble

---
+++ containers/nimble/run.sh
@@ -0,0 +1,2 @@
+/usr/bin/nimble_regutil -u $WMSPANEL_ACCOUNT -p $WMSPANEL_PASS --server-name $WMSPANEL_SERVER_NAME --recovery-id 9bbf8197-9d1b-1529-789b-012328620abc
+/usr/bin/nimble --conf-dir=/etc/nimble --log-dir=/var/log/nimble
