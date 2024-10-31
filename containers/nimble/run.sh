7ad69fdfa2345e65b59f8a5c9aef520788441c60
Paul Golmann <mail@pje-web.de>
2024-10-31 17:58:10 +0100

    using mediamtx instead

--- containers/nimble/run.sh
+++
@@ -1,2 +0,0 @@
-/usr/bin/nimble_regutil -u $WMSPANEL_ACCOUNT -p $WMSPANEL_PASS --server-name $WMSPANEL_SERVER_NAME --recovery-id 9bbf8197-9d1b-1529-789b-012328620abc
-/usr/bin/nimble --conf-dir=/etc/nimble --log-dir=/var/log/nimble
